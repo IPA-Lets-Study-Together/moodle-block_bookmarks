8c0a3477efaf47d46c9ec587a434aa3290c1bd08
Hrvoje Golčić <hrvojegolcic@gmail.com>
2015-03-12 21:23:50 +0100

    version updated

--- version.php
+++ version.php
@@ -23,4 +23,4 @@ $plugin->version = 2015031200;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
 $plugin->component = 'block_bookmarks';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.1.1'; // if you'd like to continue the development of the block, please contact hrvojegolcic@gmail.com
+$plugin->release = '1.1.2'; // if you'd like to continue the development of the block, please contact hrvojegolcic@gmail.com
